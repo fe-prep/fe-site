c63b078f94597036b0dee9655239716cef557d38
Heath Milligan <heath@heathmilligan.net>
2019-05-19 14:32:16 -0400

    Add markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,14 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/markdown`,
+        name: "markdown-pages",
+      },
+    },
+    `gatsby-transformer-remark`,
+  ]
 }
---
+++ markdown/test.md
@@ -0,0 +1,13 @@
+---
+path: "/blog/my-first-post"
+date: "2017-11-07"
+title: "My first blog post"
+---
+
+# dsdf sdfdsf
+
+### dfsdfsdfdfdfsf
+
+dsfsdf *dfsf* FDSFSDFDSF
+
+adsfsdfdasfsdf
